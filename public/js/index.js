314847305f247dcaf9ad9c233f0653b451e1f017
Brandon <captainlao@users.noreply.github.com>
2018-01-21 02:06:02 -0500

    Fix index.js reference by relocating and update to support workable browser/local server communication via socket.emit.

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,6 @@ var socket = io();
 // Use regular functions here for browser compatitibility
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'me',
-        text: 'safdfa'
-    });
 });
 
 socket.on('disconnect', function() {
@@ -18,17 +13,3 @@ socket.on('disconnect', function() {
 socket.on('newMessage', function(message) {
     console.log(message);
 });
-
-/*
-$(function() {
-    var socket = io();
-    $("form").submit(function() {
-        socket.emit("chat message", $("#m").val());
-        $("#m").val("");
-        return false;
-    });
-    socket.on("chat message", function(msg) {
-        $("#messages").append($("<li>").text(msg));
-    });
-});
-*/
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 // Normalize destination path so we don't have to go in and out of folders
-const publicPath = path.join(_dirname, '../public');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 // Create web socket server (real-time)
@@ -23,15 +23,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
     
+    // Listen for an emitted message from client and emit back to all connections
     socket.on('createMessage', (message) => {
         console.log(message);
-    });
 
-    // Create extra data (createdAt) on server side to minimize what information client is required to send
-    socket.emit('newMessage', {
-        from: 'Blah',
-        test: 'asffs',
-        createdAt: 1234
+        // createAt set here for security 
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
     });
 
     // Socket listens for client disconnect
